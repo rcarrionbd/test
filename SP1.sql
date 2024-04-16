b2eedec7680324e379692a56eaa4a859cdcf695c
rcarrionbd <136758369+rcarrionbd@users.noreply.github.com>
2024-04-16 10:25:11 -0500

    Update SP1.sql
    add sql from snowflake

--- SP1.sql
+++ SP1.sql
@@ -1,1 +1,6 @@
-select top 3 * from prod_datascience_db.streamlit_schema.example_table
+INSERT INTO prod_datascience_db.streamlit_schema.example_table_2 (id, first_name, query)
+VALUES
+    (1, 'John', 'select * from prod_datascience_db.streamlit_schema.example_table'),
+    (2, 'Jane', 'select top 1* from prod_datascience_db.streamlit_schema.example_table'),
+    (3, 'Bob', 'select top 3 * from prod_datascience_db.streamlit_schema.example_table');
+
